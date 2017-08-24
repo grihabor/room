46579d3765fc5415d5c2caf0030e0d1346cffeba
Borodin Gregory <grihabor@mail.ru>
2017-08-24 14:49:15 +0300

    open and parse json measurements

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import svgwrite
+import json
+
+FILE_MEASUREMENTS = 'measurements.json'
 
 
 def main():
-    pass
+    with open(FILE_MEASUREMENTS) as f:
+        measurements = json.load(f)
     
     
 if __name__ == '__main__':
